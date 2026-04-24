3a3a9a4c00e1ab4b178ff14cbedc80d0721141a7
Anna Mamaieva <mamaeva.anna97@gmail.com>
2026-04-24 15:59:31 +0200

    fixes for front

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000/api";
-export const AUTH_URL = "http://localhost:8000/auth";
+export const API_URL = import.meta.env.VITE_API_URL;
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
